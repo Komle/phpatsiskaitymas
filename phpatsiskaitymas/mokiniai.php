4304c4ff68f3dff222ddf7a143d7b0063cb86ca0
Komle <deizyra@hotmail.lt>
2020-09-03 11:44:23 +0300

    Add files via upload

---
+++ phpatsiskaitymas/mokiniai.php
@@ -0,0 +1,30 @@
+<?php
+$name = ['Antanas', 'Janina', 'Aldona', 'Birute', 'Vytautas', 'Jurate', 'Kamile', 'Tomas', 'Jonas', 'Ponas'];
+$lastname = ['Mandrinas', 'Panama', 'Opanija', 'Kazkokia', 'Papajus', 'Vaziuojam', 'Marko', 'Papovic', 'Mexico', 'Hilton'];
+$klase = ['5a', '5b', '5c', '6a', '6b', '6c', '7a', '7b', '7c', '8a'];
+$kodas = rand(666, 999);
+$kodas1 = rand(666, 999);
+$kodas2 = rand(666, 999);
+$kodas3 = rand(666, 999);
+$kodas4 = rand(666, 999);
+$kodas5 = rand(666, 999);
+$kodas6 = rand(666, 999);
+$kodas7 = rand(666, 999);
+$kodas8 = rand(666, 999);
+$kodas9 = rand(666, 999);
+$date= date("Y/m/d");
+$matematika = [5, 6, 7, 8, 9, 10, 7, 9, 6, 3];
+$informatika = [8, 4, 6, 7, 8, 5, 10, 6, 4, 9];
+$anglukalba = [6, 8, 3, 4, 5, 7, 3, 2, 4, 8];
+
+$vidurkis = (($matematika[0] + $informatika[0] + $anglukalba[0]) / 3);
+$vidurkis1 = (($matematika[1] + $informatika[1] + $anglukalba[1]) / 3);
+$vidurkis2 = (($matematika[2] + $informatika[2] + $anglukalba[2]) / 3);
+$vidurkis3 = (($matematika[3] + $informatika[3] + $anglukalba[3]) / 3);
+$vidurkis4 = (($matematika[4] + $informatika[4] + $anglukalba[4]) / 3);
+$vidurkis5 = (($matematika[5] + $informatika[5] + $anglukalba[5]) / 3);
+$vidurkis6 = (($matematika[6] + $informatika[6] + $anglukalba[6]) / 3);
+$vidurkis7 = (($matematika[7] + $informatika[7] + $anglukalba[7]) / 3);
+$vidurkis8 = (($matematika[8] + $informatika[8] + $anglukalba[8]) / 3);
+$vidurkis9 = (($matematika[9] + $informatika[9] + $anglukalba[9]) / 3);
+
